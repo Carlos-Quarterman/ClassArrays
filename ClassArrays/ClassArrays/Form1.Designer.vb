077d087511fa912c717dfbcbf4ef357da3c7d21e
Carlos Quarterman <carlos.quarterman@outlook.com>
2022-10-24 15:21:05 -0400

    Add files via upload

---
+++ ClassArrays/ClassArrays/Form1.Designer.vb
@@ -0,0 +1,161 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.PrintForm1 = New Microsoft.VisualBasic.PowerPacks.Printing.PrintForm(Me.components)
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Font = New System.Drawing.Font("Courier New", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Location = New System.Drawing.Point(6, 19)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(355, 516)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.SteelBlue
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(392, 552)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(143, 36)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Legit Check"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.ListBox2)
+        Me.GroupBox1.Controls.Add(Me.ListBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(83, 43)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(778, 456)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Bubble Sort"
+        '
+        'ListBox2
+        '
+        Me.ListBox2.Font = New System.Drawing.Font("Courier New", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.ItemHeight = 16
+        Me.ListBox2.Location = New System.Drawing.Point(407, 19)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(355, 516)
+        Me.ListBox2.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.SteelBlue
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(359, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(204, 37)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Joseph Clark"
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.SteelBlue
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(117, 552)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(115, 36)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Print"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.SteelBlue
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(678, 552)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(127, 36)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Exit"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'PrintForm1
+        '
+        Me.PrintForm1.DocumentName = "document"
+        Me.PrintForm1.Form = Me
+        Me.PrintForm1.PrintAction = System.Drawing.Printing.PrintAction.PrintToPrinter
+        Me.PrintForm1.PrinterSettings = CType(resources.GetObject("PrintForm1.PrinterSettings"), System.Drawing.Printing.PrinterSettings)
+        Me.PrintForm1.PrintFileName = Nothing
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.SteelBlue
+        Me.Button4.Location = New System.Drawing.Point(392, 620)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(132, 23)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "Clear"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.SlateGray
+        Me.ClientSize = New System.Drawing.Size(906, 655)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Bubble Sort"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents PrintForm1 As Microsoft.VisualBasic.PowerPacks.Printing.PrintForm
+    Friend WithEvents ListBox2 As System.Windows.Forms.ListBox
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+
+End Class
---
+++ ClassArrays/ClassArrays/Form1.vb
@@ -0,0 +1,71 @@
+﻿Public Class Form1
+    'Joseph Clark
+    'April 28, 2016
+    'Class Array Extra Credit
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Lastname() As String = {"Broomfield", "Brown", "Campbell", "Canteen", "Cason", "Clark", "Clemons", "Collins", "Craig", "Dalmida", "Daniel", "Harris", "Hunter", "Kemp", "King", "Mcclendon", "Mesamours", "Mitchell", "Quarterman", "Rouse", "Suggs", "West", "Whitaker", "White", "Williams"}
+        Dim Firstname() As String = {"Lawrence", "Mesha", "Michelle", "Alayah", "Jenae", "Joseph", "Travis", "Melody", "Cedric", "Joel", "Jasmine", "Robert", "Anthony", "Dexter", "Julious", "Kevin", "Rolwilde", "Kyle", "Carlos", "Bry'on", "Janika", "Keyonna", "Jaron", "Robbie", "Eric"}
+
+        Dim Lastname1() As String = {"Brown", "Campbell", "Williams", "Quarterman", "King", "Rouse", "Mitchell", "Hunter", "Kemp", "Craig", "Clark", "Broomfield", "Dalmida", "Suggs", "West", "Collins", "Mesamours", "McClendon", "Harris", "Cason", "White", "Clemons", "Daniel", "Whitaker"}
+        Dim Firstname1() As String = {"Mesha", "Michelle", "Eric", "Carlos", "Julious", "Bry'On", "Kyle", "Anthony", "Dexter", "Cedric", "Joseph", "Lawrence", "Joel", "Janika", "Keyona", "Melody", "Rolwilde", "Kevin", "Robert", "Jenae", "Robbie", "Travis", "Jasmine", "Jaron"}
+
+        Dim temp As String
+        Dim temp10 As String
+        Dim counter As Integer = 0
+        Dim counter1 As Integer = 0
+
+
+        ListBox1.Items.Add("Sorted")
+        ListBox1.Items.Add("")
+
+        For h As Integer = 0 To 23
+            counter = counter + 1
+            ListBox1.Items.Add(counter & "." & Firstname(h) & "    " & vbTab & Lastname(h))
+            'ListBox1.Items.Add("")
+        Next
+        For i = 23 To 0 Step -1
+            For h = 0 To i
+                If Lastname(h) < Lastname(h + 1) Then
+                    temp = Lastname(h)
+                    Lastname(h) = Lastname(h + 1)
+                    Lastname(h + 1) = temp
+
+                    temp10 = Firstname(h)
+                    Firstname(h) = Firstname(h + 1)
+                    Firstname(h + 1) = temp10
+
+                End If
+            Next
+        Next
+
+        ListBox2.Items.Add("UnSorted")
+        ListBox2.Items.Add("")
+        For h As Integer = 0 To 23
+            counter1 = counter1 + 1
+            ListBox2.Items.Add(counter1 & "." & Firstname1(h) & "    " & vbTab & Lastname1(h))
+            'ListBox2.Items.Add("")
+        Next
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        End
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        PrintForm1.PrintAction = Printing.PrintAction.PrintToPreview
+        Me.PrintForm1.PrinterSettings.DefaultPageSettings.Landscape = True
+        PrintForm1.Print()
+    End Sub
+
+  
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        ListBox1.Items.Clear()
+        ListBox2.Items.Clear()
+    End Sub
+End Class
